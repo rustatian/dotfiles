c14ebb07b2e4033415face03fe39e9d624b0daee
Valery Piashchynski <piashchynski.valery@gmail.com>
2024-03-12 15:44:42 +0100

    chore: update clangd filetypes
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -23,7 +23,7 @@ require("neodev").setup({
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local on_attach = function(client, bufnr)
@@ -84,6 +84,7 @@ lspconfig.clangd.setup({
 	settings = {
 		clangd = {},
 	},
+	filetypes = { "c", "cpp", "h" },
 })
 
 lspconfig.bufls.setup({
