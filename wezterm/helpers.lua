785d2b66293901a04901c98dbc92b98f72605941
Valery Piashchynski <piashchynski.valery@gmail.com>
2024-05-19 22:23:46 +0200

    chore: update font
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- wezterm/helpers.lua
+++ wezterm/helpers.lua
@@ -22,7 +22,7 @@ end)
 -- the config is for them to export an `apply_to_config`
 -- function that accepts the config object, like this:
 function module.apply_to_config(config)
-	config.font = wezterm.font("Berkeley Mono", { weight = "Bold", italic = false })
+	config.font = wezterm.font("Monaspace Neon SemiWide Medium", { weight = "Medium", italic = false })
 	config.font_size = 14.0
 	config.color_scheme = "Kanagawa (Gogh)"
 	config.line_height = 1
