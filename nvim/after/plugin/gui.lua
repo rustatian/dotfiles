0901a57fb3b2c0e0c4d36956ddc49d77e2031515
Valery Piashchynski <piashchynski.valery@gmail.com>
2024-10-02 15:21:21 +0200

    chore: update theme
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- nvim/after/plugin/gui.lua
+++ nvim/after/plugin/gui.lua
@@ -1,58 +1,64 @@
-require("catppuccin").setup({
-	flavour = "auto", -- latte, frappe, macchiato, mocha
-	background = { -- :h background
-		light = "latte",
-		dark = "mocha",
-	},
-	transparent_background = false, -- disables setting the background color.
-	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-	term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-	dim_inactive = {
-		enabled = false, -- dims the background color of inactive window
-		shade = "dark",
-		percentage = 0.15, -- percentage of the shade to apply to the inactive window
-	},
-	no_italic = true, -- Force no italic
-	no_bold = false, -- Force no bold
-	no_underline = false, -- Force no underline
-	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-		comments = {}, -- Change the style of comments
-		conditionals = {},
-		loops = {},
-		functions = {},
-		keywords = {},
-		strings = {},
-		variables = {},
-		numbers = {},
-		booleans = {},
-		properties = {},
-		types = {},
-		operators = {},
-		-- miscs = {}, -- Uncomment to turn off hard-coded styles
-	},
-	color_overrides = {},
-	custom_highlights = {},
-	default_integrations = true,
-	integrations = {
-		cmp = true,
-		gitsigns = true,
-		nvimtree = true,
-		treesitter = true,
-		notify = false,
-		mini = {
-			enabled = true,
-			indentscope_color = "",
+require("tokyonight").setup({
+	-- use the night style
+	style = "night",
+	-- disable italic for functions
+	styles = {
+		comments = {
+			italic = false,
 		},
-		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+		functions = {
+			italic = false,
+		},
+		keywords = {
+			italic = false,
+		},
+		variables = {
+			italic = false,
+		},
 	},
+	-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+	on_colors = function(colors)
+		colors.hint = colors.orange
+		colors.error = "#ff0000"
+	end,
+	on_highlights = function(hl, c)
+		local prompt = "#2d3149"
+		hl.TelescopeNormal = {
+			bg = c.bg_dark,
+			fg = c.fg_dark,
+		}
+		hl.TelescopeBorder = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+		hl.TelescopePromptNormal = {
+			bg = prompt,
+		}
+		hl.TelescopePromptBorder = {
+			bg = prompt,
+			fg = prompt,
+		}
+		hl.TelescopePromptTitle = {
+			bg = prompt,
+			fg = prompt,
+		}
+		hl.TelescopePreviewTitle = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+		hl.TelescopeResultsTitle = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+	end,
 })
 
 -- setup must be called before loading
 vim.o.background = "dark"
 vim.cmd.hi("Comment gui=none")
-vim.cmd.colorscheme("catppuccin")
+vim.cmd([[colorscheme tokyonight]])
 
 require("barbecue").setup({
-	theme = "catppuccin",
+	theme = "tokyonight",
 	attach_navic = false, -- prevent barbecue from automatically attaching nvim-navic
 })
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -31,6 +31,10 @@ return {
 		opts = {},
 	},
 	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -49,7 +53,7 @@ return {
 		},
 	},
 	{
-		"catppuccin/nvim",
+		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 	},
@@ -173,20 +177,6 @@ return {
 			-- - sd'   - [S]urround [D]elete [']quotes
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require("mini.surround").setup()
-			-- Simple and easy statusline.
-			--  You could remove this setup call if you don't like it,
-			--  and try some other statusline plugin
-			local statusline = require("mini.statusline")
-			-- set use_icons to true if you have a Nerd Font
-			statusline.setup({ use_icons = vim.g.have_nerd_font })
-
-			-- You can configure sections in the statusline by overriding their
-			-- default behavior. For example, here we set the section for
-			-- cursor location to LINE:COLUMN
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
 		end,
 	},
 	-- WakaTime
