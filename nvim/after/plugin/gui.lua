1081b904864e55e6656177f431bb87af530af1d8
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-06-04 12:14:03 +0200

    chore: update themes
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- nvim/after/plugin/gui.lua
+++ nvim/after/plugin/gui.lua
@@ -1,49 +1,49 @@
-require('github-theme').setup({
-	options = {
-		-- Compiled file's destination location
-		compile_path = vim.fn.stdpath('cache') .. '/github-theme',
-		compile_file_suffix = '_compiled', -- Compiled file suffix
-		hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
-		hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-		transparent = false, -- Disable setting bg (make neovim's background transparent)
-		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-		dim_inactive = false, -- Non focused panes set to alternative background
-		module_default = true, -- Default enable value for modules
-		styles = {           -- Style to be applied to different syntax groups
-			comments = 'NONE', -- Value is any valid attr-list value `:help attr-list`
-			functions = 'NONE',
-			keywords = 'NONE',
-			variables = 'NONE',
-			conditionals = 'NONE',
-			constants = 'NONE',
-			numbers = 'NONE',
-			operators = 'NONE',
-			strings = 'NONE',
-			types = 'NONE',
-		},
-		inverse = { -- Inverse highlight for different types
-			match_paren = false,
-			visual = false,
-			search = false,
-		},
-		darken = { -- Darken floating windows and sidebar-like windows
-			floats = true,
-			sidebars = {
-				enable = true,
-				list = {}, -- Apply dark background to specific windows
-			},
-		},
-		modules = { -- List of various plugins and additional options
-		},
+-- setup must be called before loading
+require("tokyonight").setup({
+	styles = {
+		-- Style to be applied to different syntax groups
+		-- Value is any valid attr-list value for `:help nvim_set_hl`
+		comments = { italic = false },
+		keywords = { italic = false },
+		functions = { italic = false },
+		variables = { italic = false },
+		-- Background styles. Can be "dark", "transparent" or "normal"
+		sidebars = "dark", -- style for sidebars, see below
+		floats = "dark", -- style for floating windows
 	},
-	palettes = {},
-	specs = {},
-	groups = {},
+	on_highlights = function(hl, c)
+		local prompt = "#2d3149"
+		hl.TelescopeNormal = {
+			bg = c.bg_dark,
+			fg = c.fg_dark,
+		}
+		hl.TelescopeBorder = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+		hl.TelescopePromptNormal = {
+			bg = prompt,
+		}
+		hl.TelescopePromptBorder = {
+			bg = prompt,
+			fg = prompt,
+		}
+		hl.TelescopePromptTitle = {
+			bg = prompt,
+			fg = prompt,
+		}
+		hl.TelescopePreviewTitle = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+		hl.TelescopeResultsTitle = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+	end,
 })
 
--- setup must be called before loading
-vim.cmd('colorscheme github_dark_dimmed')
-vim.o.background = "dark"
+vim.cmd('colorscheme tokyonight-night')
 vim.cmd.hi("Comment gui=none")
 
 require("barbecue").setup({
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -92,8 +92,10 @@ return {
 		},
 	},
 	{
-		"projekt0n/github-nvim-theme",
+		"folke/tokyonight.nvim",
+		lazy = false,
 		priority = 1000,
+		opts = {},
 	},
 	{
 		"j-hui/fidget.nvim",
