66131949f5ea0803e31a2b0c09fb67523b78a3c6
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-07-27 20:05:09 +0200

    chore: theming update
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- .config/nvim/after/plugin/gui.lua
+++ .config/nvim/after/plugin/gui.lua
@@ -1,31 +1,57 @@
-require("gruvbox").setup({
-	terminal_colors = true, -- add neovim terminal colors
-	undercurl = true,
-	underline = true,
-	bold = true,
-	italic = {
-		strings = false,
-		emphasis = false,
-		comments = false,
-		operators = false,
-		folds = true,
+require("catppuccin").setup({
+	flavour = "auto", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = "latte",
+		dark = "mocha",
 	},
-	strikethrough = true,
-	invert_selection = false,
-	invert_signs = false,
-	invert_tabline = false,
-	inverse = true, -- invert background for search, diffs, statuslines and errors
-	contrast = "", -- can be "hard", "soft" or empty string
-	palette_overrides = {},
-	overrides = {},
-	dim_inactive = false,
-	transparent_mode = false,
+	transparent_background = false, -- disables setting the background color.
+	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	term_colors = false,        -- sets terminal colors (e.g. `g:terminal_color_0`)
+	dim_inactive = {
+		enabled = false,    -- dims the background color of inactive window
+		shade = "dark",
+		percentage = 0.15,  -- percentage of the shade to apply to the inactive window
+	},
+	no_italic = false,          -- Force no italic
+	no_bold = false,            -- Force no bold
+	no_underline = false,       -- Force no underline
+	styles = {                  -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = {},      -- Change the style of comments
+		conditionals = {},
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+		-- miscs = {}, -- Uncomment to turn off hard-coded styles
+	},
+	color_overrides = {},
+	custom_highlights = {},
+	default_integrations = true,
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		treesitter = true,
+		notify = false,
+		mini = {
+			enabled = true,
+			indentscope_color = "",
+		},
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
 })
 
-vim.cmd("colorscheme gruvbox")
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
 vim.cmd.hi("Comment gui=none")
 
 require("barbecue").setup({
-	theme = "gruvbox", -- theme
+	theme = "catppuccin", -- theme
 	attach_navic = false, -- prevent barbecue from automatically attaching nvim-navic
 })
--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -70,9 +70,9 @@ return {
 	-- GUI enhancements --
 	-----------------------
 	{
-		"ellisonleao/gruvbox.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000,
-		config = true,
 	},
 	{
 		"stevearc/dressing.nvim",
