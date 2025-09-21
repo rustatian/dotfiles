aa20b1f8a58fa7abd769c957a28435437c8e7db3
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-09-21 16:48:44 +0200

    chore: small update
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -6,9 +6,17 @@ require("conform").setup({
 	},
 	formatters_by_ft = {
 		lua = { "stylua" },
-		python = { "isort", "black" },
+		python = { "ruff", "isort" },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		rust = { "clippy" },
 		go = { "gofmt", "gofumpt", stop_after_first = true },
 	},
+	keys = {
+		"<leader>cF",
+		function()
+			require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+		end,
+		mode = { "n", "v" },
+		desc = "Format Injected Langs",
+	}
 })
