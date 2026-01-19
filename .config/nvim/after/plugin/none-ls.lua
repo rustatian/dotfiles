0837573461bb9578c5ea47800c06eadc8b9240d8
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-01-19 23:08:25 +0100

    chore: additional tools none-ls
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- .config/nvim/after/plugin/none-ls.lua
+++ .config/nvim/after/plugin/none-ls.lua
@@ -3,6 +3,7 @@ local u = require("null-ls.utils")
 
 null_ls.setup({
 	sources = {
+		-- Python type checking
 		null_ls.builtins.diagnostics.mypy.with({
 			extra_args = function(params)
 				local args = {}
@@ -26,5 +27,29 @@ null_ls.setup({
 				return args
 			end,
 		}),
+
+		-- Spelling/typos
+		null_ls.builtins.diagnostics.codespell,
+
+		-- Makefile
+		null_ls.builtins.diagnostics.checkmake,
+
+		-- Markdown
+		null_ls.builtins.diagnostics.markdownlint,
+
+		-- YAML
+		null_ls.builtins.diagnostics.yamllint,
+
+		-- Security - detect secrets
+		null_ls.builtins.diagnostics.gitleaks,
+
+		-- Git commit messages
+		null_ls.builtins.diagnostics.gitlint,
+
+		-- Protobuf
+		null_ls.builtins.diagnostics.protolint,
+
+		-- Security/code analysis
+		null_ls.builtins.diagnostics.semgrep,
 	},
 })
