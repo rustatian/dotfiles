e8a210070b62bac5948eb7dfb121913726195b7b
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-02-08 15:56:25 +0100

    chore: update background
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -1,13 +1,17 @@
 require("bufferline").setup({
 	options = {
 		diagnostics = "nvim_lsp",
+		color_icons = true,
+		show_buffer_close_icons = true,
+		show_close_icon = true,
+		show_buffer_icons = true,
 		offsets = {
 			{
 				filetype = "NvimTree",
 				text = "File Explorer",
 				highlight = "Directory",
-				separator = true,
 			},
 		},
 	},
+	highlights = require("catppuccin.special.bufferline").get_theme(),
 })
